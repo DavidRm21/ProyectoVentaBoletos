f4ad414f3a1e1c9361ae308f2c01639fc1797ca3
DavidRm21 <cdavidromero214@gmail.com>
2024-11-03 12:22:35 -0500

    crea carpeta view moel

---
+++ ViewModel/LoginPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ProyectoFinalM.ViewModel;
+
+public class LoginPageViewModel
+{
+    public async void OnComprar(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//MoviesPage");
+    }
+}
